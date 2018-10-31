db13bc6ef8f94aaa4aa75fe8725698e2378892ba
gayatri P <gaya3p13@gmail.com>
2018-10-31 20:27:41 +0530

    vanillafied 🍦

--- assets/app.js
+++ assets/app.js
@@ -1,51 +1,84 @@
 let selectedToppings = []
 
 const totalCostBox = document.getElementById('total-cost')
-const toppingsCostBox  = document.getElementById('toppings-cost')
+const toppingsCostBox = document.getElementById('toppings-cost')
 const cheeseCostBox = document.getElementById('cheese-cost')
 const gstCostBox = document.getElementById('gst-cost')
 const grandCostBox = document.getElementById('grand-cost')
 const quantity = document.getElementById('quantity')
+const toppingDivs = document.querySelectorAll('.topping')
+const crustDivs = document.querySelectorAll('.crust')
+
+const toppingPrices = {
+    mushroom: 13,
+    onion: 30,
+    paneer: 45,
+    paparika:23,
+    jalapeno:23,
+    greenOlives: 67,
+    goldenCorn: 34,
+    capsicum: 45,
+    periPeriChicken:34,
+    barbeque:56,
+    sausage:23,
+    chickenTikka:76,
+    grilledChickenRasher:23
+}
+
+const crustPrices = {
+    classicHandTossed: 34,
+    cheeseBurst:23,
+    wheatThinCrust:43,
+    freshPan:34,
+    newHandTossed:43
+}
 
 let totalCost, toppingsCost, cheeseCost, gstCost, grandCost;
 
-const RN = num => {return Math.round(num * 100) / 100}
+const RN = num => { return Math.round(num * 100) / 100 }
+const toggleClass = (item, c) => {
+    item.classList.contains(c) ? item.classList.remove(c) : item.classList.add(c)
+}
 
 // TOPPING SELECTION (AS MANY AS YOU WANT)
-$('.topping').click(e => {
-    let target = e.target
-    
-    if( !(target.classList.contains('topping')) ) {
-        target = target.parentNode
-    }
+toppingDivs.forEach(div =>
+    div.addEventListener('click', e => {
+        let target = e.target
 
-    $(target).toggleClass('on-the-pizza')
-    let toppingName = target.querySelector('span').innerText;
+        if (!(target.classList.contains('topping'))) {
+            target = target.parentNode
+        }
 
-    if (target.classList.contains('on-the-pizza')) {
-        selectedToppings.push(toppingName)
-    }
-    else {
-        selectedToppings.splice(selectedToppings.indexOf(toppingName), 1)
-    }
-    updateBill()
-    updateImage(selectedToppings)
-})
+        toggleClass(target, 'on-the-pizza')
+        let toppingName = target.querySelector('span').innerText;
+
+        if (target.classList.contains('on-the-pizza')) {
+            selectedToppings.push(toppingName)
+        }
+        else {
+            selectedToppings.splice(selectedToppings.indexOf(toppingName), 1)
+        }
+        updateBill()
+        updateImage(selectedToppings)
+    })
+)
 
 // CRUST SELECTION (ONE AT ONCE)
-$('.crust').click(e => {
-    $('.selected-crust').removeClass('selected-crust')
-    $(e.target).addClass('selected-crust')
+crustDivs.forEach(div => {
+    div.addEventListener('click', e => {
+        document.querySelector('.selected-crust').classList.remove('selected-crust')
+        e.target.classList.add('selected-crust')
+    })
 })
 
-// SWITCH B/W VEG &B NON-VEG
+// SWITCH B/W VEG & NON-VEG
 const openGenre = (e, genre) => {
-    $('.toppings-genre.active').removeClass('active')
-    $(e.target).addClass('active')
+    document.querySelector('.toppings-genre.active').classList.remove('active')
+    e.target.classList.add('active')
 
-    $('.toppings-body-child.active-genre').removeClass('active-genre');
+    document.querySelector('.toppings-body-child.active-genre').classList.remove('active-genre');
 
-    (genre === 'veg') ? $('.toppings-body-veg').addClass('active-genre') : $('.toppings-body-non').addClass('active-genre')
+    (genre === 'veg') ? document.querySelector('.toppings-body-veg').classList.add('active-genre') : document.querySelector('.toppings-body-non').classList.add('active-genre')
 }
 
 // UPDATING BILL
@@ -55,26 +88,25 @@ const updateBill = () => {
     // ASSUMING ALL TOPPINGS TO COST THE SAME
     let toppingsCount = selectedToppings.length
 
-    totalCost = 70*n
-    totalCostBox.innerText = '₹'+totalCost
+    totalCost = 70 * n
+    totalCostBox.innerText = '₹' + totalCost
 
     toppingsCost = toppingsCount * 15 * n
-    toppingsCostBox.innerText = '₹'+toppingsCost
+    toppingsCostBox.innerText = '₹' + toppingsCost
 
-    cheeseCost = document.getElementById('extra-cheese').checked ? 15*n : 0
-    cheeseCostBox.innerText =  '₹'+cheeseCost
+    cheeseCost = document.getElementById('extra-cheese').checked ? 15 * n : 0
+    cheeseCostBox.innerText = '₹' + cheeseCost
 
-    gstCost = RN(0.05 * (cheeseCost+toppingsCost+totalCost))
-    gstCostBox.innerText = '₹'+gstCost
+    gstCost = RN(0.05 * (cheeseCost + toppingsCost + totalCost))
+    gstCostBox.innerText = '₹' + gstCost
 
     grandCost = gstCost + cheeseCost + toppingsCost + totalCost
-    grandCostBox.innerText = '₹'+grandCost
+    grandCostBox.innerText = '₹' + grandCost
 }
-
 const updateImage = (toppings) => {
-    $('.yes').removeClass('yes');
+    document.querySelectorAll('.yes').forEach(img => img.classList.remove('yes'))
     toppings.forEach(topping => {
-        toppingID = (topping.toLowerCase()).split(' ').join('-');
-        $('#'+toppingID).addClass('yes')
+        toppingID = (topping.toLowerCase()).split(' ').join('-')
+        document.querySelector('#' + toppingID).classList.add('yes')
     })
 }
